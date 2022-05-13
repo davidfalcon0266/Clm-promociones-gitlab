ad69cf61ecb9a56804894a360db78743a9888e3d
david <david.026697@gmail.com>
2022-05-12 22:50:23 -0400

    Limpieza codigo

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { UsuarioLogin } from '../interfaces/user-login';
 import { UserRegister } from '../interfaces/user-register';
 
